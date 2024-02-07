65821edf748470ada237508267668a6d9c970026
Olga Andreeva <124622579+oandreeva-nv@users.noreply.github.com>
2024-02-07 10:52:42 -0800

    Updating vLLM tests to handle changes between vLLM version 0.2.3 and 0.3.0 (#30)

--- ci/L0_backend_vllm/accuracy_test/accuracy_test.py
+++ ci/L0_backend_vllm/accuracy_test/accuracy_test.py
@@ -1,4 +1,4 @@
-# Copyright 2023, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# Copyright 2023-2024, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
@@ -24,7 +24,9 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+import argparse
 import asyncio
+import pickle
 import sys
 import unittest
 from functools import partial
@@ -39,12 +41,24 @@ from vllm.utils import random_uuid
 sys.path.append("../../common")
 from test_util import TestResultCollector, UserData, callback, create_vllm_request
 
+VLLM_ENGINE_CONFIG = {
+    "model": "facebook/opt-125m",
+    "gpu_memory_utilization": 0.3,
+}
+
+
+PROMPTS = [
+    "The most dangerous animal is",
+    "The capital of France is",
+    "The future of AI is",
+]
+
+SAMPLING_PARAMETERS = {"temperature": 0, "top_p": 1}
+
 
 async def generate_python_vllm_output(prompt, llm_engine):
     request_id = random_uuid()
-    sampling_parameters = {"temperature": 0, "top_p": 1}
-    sampling_params = SamplingParams(**sampling_parameters)
-
+    sampling_params = SamplingParams(**SAMPLING_PARAMETERS)
     python_vllm_output = None
     last_output = None
 
@@ -59,50 +73,68 @@ async def generate_python_vllm_output(prompt, llm_engine):
     return python_vllm_output
 
 
+def prepare_vllm_baseline_outputs():
+    """
+    Helper function that starts async vLLM engine and generates output for each
+    prompt in `PROMPTS`. Saves resulted baselines in `vllm_baseline_output.pkl`
+    for further use.
+    """
+    llm_engine = AsyncLLMEngine.from_engine_args(AsyncEngineArgs(**VLLM_ENGINE_CONFIG))
+    python_vllm_output = []
+    for i in range(len(PROMPTS)):
+        python_vllm_output.extend(
+            asyncio.run(generate_python_vllm_output(PROMPTS[i], llm_engine))
+        )
+
+    with open("vllm_baseline_output.pkl", "wb") as f:
+        pickle.dump(python_vllm_output, f)
+
+    return
+
+
 class VLLMTritonAccuracyTest(TestResultCollector):
     def setUp(self):
         self.triton_client = grpcclient.InferenceServerClient(url="localhost:8001")
-        vllm_engine_config = {
-            "model": "facebook/opt-125m",
-            "gpu_memory_utilization": 0.3,
-        }
-
-        self.llm_engine = AsyncLLMEngine.from_engine_args(
-            AsyncEngineArgs(**vllm_engine_config)
-        )
         self.vllm_model_name = "vllm_opt"
+        self.python_vllm_output = []
+        with open("vllm_baseline_output.pkl", "rb") as f:
+            self.python_vllm_output = pickle.load(f)
+
+        self.assertNotEqual(
+            self.python_vllm_output,
+            [],
+            "Loaded baseline outputs' list should not be empty",
+        )
+        self.assertIsNotNone(
+            self.python_vllm_output, "Loaded baseline outputs' list should not be None"
+        )
+        self.assertEqual(
+            len(self.python_vllm_output),
+            len(PROMPTS),
+            "Unexpected number of baseline outputs loaded, expected {}, but got {}".format(
+                len(PROMPTS), len(self.python_vllm_output)
+            ),
+        )
 
     def test_vllm_model(self):
         user_data = UserData()
         stream = False
-        prompts = [
-            "The most dangerous animal is",
-            "The capital of France is",
-            "The future of AI is",
-        ]
-        number_of_vllm_reqs = len(prompts)
-        sampling_parameters = {"temperature": "0", "top_p": "1"}
-        python_vllm_output = []
         triton_vllm_output = []
 
         self.triton_client.start_stream(callback=partial(callback, user_data))
-        for i in range(number_of_vllm_reqs):
+        for i in range(len(PROMPTS)):
             request_data = create_vllm_request(
-                prompts[i], i, stream, sampling_parameters, self.vllm_model_name
+                PROMPTS[i], i, stream, SAMPLING_PARAMETERS, self.vllm_model_name
             )
             self.triton_client.async_stream_infer(
                 model_name=self.vllm_model_name,
                 request_id=request_data["request_id"],
                 inputs=request_data["inputs"],
                 outputs=request_data["outputs"],
-                parameters=sampling_parameters,
+                parameters=SAMPLING_PARAMETERS,
             )
 
-            python_vllm_output.extend(
-                asyncio.run(generate_python_vllm_output(prompts[i], self.llm_engine))
-            )
-
-        for i in range(number_of_vllm_reqs):
+        for i in range(len(PROMPTS)):
             result = user_data._completed_requests.get()
             self.assertIsNot(type(result), InferenceServerException, str(result))
 
@@ -112,11 +144,24 @@ class VLLMTritonAccuracyTest(TestResultCollector):
             triton_vllm_output.extend(output)
 
         self.triton_client.stop_stream()
-        self.assertEqual(python_vllm_output, triton_vllm_output)
+        self.assertEqual(self.python_vllm_output.sort(), triton_vllm_output.sort())
 
     def tearDown(self):
         self.triton_client.close()
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--generate-baseline",
+        action="store_true",
+        required=False,
+        default=False,
+        help="Generates baseline output for accuracy tests",
+    )
+    FLAGS = parser.parse_args()
+    if FLAGS.generate_baseline:
+        prepare_vllm_baseline_outputs()
+        exit(0)
+
     unittest.main()
