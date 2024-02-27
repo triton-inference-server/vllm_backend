cf5797fc13c632dede9226e9b89c747395076a2a
Mikhail Khludnev <mkhludnev@users.noreply.github.com>
2024-02-27 22:44:21 +0300

    Update samples/client.py
    Co-authored-by: Olga Andreeva <124622579+oandreeva-nv@users.noreply.github.com>

--- samples/client.py
+++ samples/client.py
@@ -94,7 +94,11 @@ class LLMClient:
         # including `temperature`, `top_p`, top_k`, `max_tokens`, `early_stopping`.
         # Full list available at:
         # https://github.com/vllmproject/vllm/blob/5255d99dc595f9ae7647842242d6542aa4145a4f/vllm/sampling_params.py#L23
-        sampling_parameters = {"temperature": "0.1", "top_p": "0.95", "max_tokens":"100"}
+        sampling_parameters = {
+            "temperature": "0.1",
+            "top_p": "0.95",
+            "max_tokens": "100",
+        }
         with open(self._flags.input_prompts, "r") as file:
             print(f"Loading inputs from `{self._flags.input_prompts}`...")
             prompts = file.readlines()
