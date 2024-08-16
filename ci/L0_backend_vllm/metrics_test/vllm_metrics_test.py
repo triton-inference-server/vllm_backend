2d7488d71c5052f25678f48f3d127f176e10e1ba
Yingge He <yinggeh@nvidia.com>
2024-08-16 14:59:26 -0700

    Change assertTrue(a>b) to assertGreater(a, b)

--- ci/L0_backend_vllm/metrics_test/vllm_metrics_test.py
+++ ci/L0_backend_vllm/metrics_test/vllm_metrics_test.py
@@ -127,11 +127,11 @@ class VLLMTritonMetricsTest(TestResultCollector):
 
         # vllm:time_to_first_token_seconds
         self.assertEqual(metrics_dict["vllm:time_to_first_token_seconds_count"], 3)
-        self.assertTrue(metrics_dict["vllm:time_to_first_token_seconds_sum"] > 0)
+        self.assertGreater(metrics_dict["vllm:time_to_first_token_seconds_sum"], 0)
         self.assertEqual(metrics_dict["vllm:time_to_first_token_seconds_bucket"], 3)
         # vllm:time_per_output_token_seconds
         self.assertEqual(metrics_dict["vllm:time_per_output_token_seconds_count"], 45)
-        self.assertTrue(metrics_dict["vllm:time_per_output_token_seconds_sum"] > 0)
+        self.assertGreater(metrics_dict["vllm:time_per_output_token_seconds_sum"], 0)
         self.assertEqual(metrics_dict["vllm:time_per_output_token_seconds_bucket"], 45)
 
     def test_vllm_metrics_disabled(self):
